caa77d26c9c3808d478a4c3946d84a84f11b358a
khomasl <gymnasium.yv@gmail.com>
2024-05-04 22:19:09 +0300

    fix: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,13 @@
-import './global.css'
+import '../styles/global.css'
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 
-import { baseUrl } from './utils/sitemap'
-import Navbar from './components/nav'
-import Footer from './components/footer'
+import { baseUrl } from '../utils/sitemap'
+import Navbar from '../components/nav'
+import Footer from '../components/footer'
 
 export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
@@ -55,7 +55,7 @@ export default function RootLayout({
         GeistMono.variable
       )}
     >
-      <body className="antialiased max-w-xl mx-4 mt-8 lg:mx-auto">
+      <body className="antialiased max-w-[1000px] px-20px mx-4 mt-8 lg:mx-auto">
         <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
           <Navbar />
           {children}
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import Image from 'next/image'
-import logo from '../../public/logo.png'
+import logo from '../public/logo.png'
 
 const navItems = {
   '/': {
@@ -19,7 +19,7 @@ export default function Navbar() {
     <aside className="ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20 w-full">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row items-start justify-between relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
           <Image
