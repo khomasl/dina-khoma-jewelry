73e718f41a2f7969872a5be8d9dec6a73dd47880
oleksandrkever <oleksandr.kever@fourmeta.com>
2024-05-17 21:18:41 +0300

    🚀 Add sharp dependency to package.json

--- shared/ui/header/burger/index.tsx
+++
@@ -1,9 +0,0 @@
-const Burger = () => (
-  <div className="flex md:hidden flex-col justify-between w-8 h-8">
-    <div className="border border-b"></div>
-    <div className="border border-b"></div>
-    <div className="border border-b"></div>
-  </div>
-);
-
-export default Burger;
