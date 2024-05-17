6df5057f51a2a686a20a685a21b753d53b2a2a47
khomasl <gymnasium.yv@gmail.com>
2024-05-17 14:04:51 +0300

    added photo, fix: header, footer

---
+++ shared/index.ts
@@ -0,0 +1,7 @@
+import Burger from "shared/ui/header/burger";
+import Logo from "shared/ui/header/logo";
+import Menu from "shared/ui/header/nav-menu";
+import Phone from "shared/ui/header/phone";
+import Socials from "shared/ui/header/nav-socials";
+
+export {Burger, Logo, Menu, Phone, Socials}
---
+++ shared/ui/header/nav-socials/index.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import Image from "next/image";
+import { CONTENT } from "../../../../utils/constants";
+
+const { socials } = CONTENT.header;
+
+const Socials = () => (
+  <div className="hidden md:flex flex-row space-x-1 ml-5">
+    {socials.map((social, idx) => {
+      return (
+        <Link
+          key={social.title}
+          href={social.link}
+          className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative mr-2.5"
+        >
+          <>
+            {/* {idx===0 && <Instagram />} */}
+            {/* {idx===1 && <Facebook />} */}
+            {/* {idx===2 && <Telegram className="hover:bg-red-400 hover:stroke-red-100" */}
+            {/* />} */}
+            <Image
+              alt={social.title}
+              src={require(`/public/svg/${social.src}?url`)}
+              // src={instagramUrl}
+              width={50}
+              height={50}
+              style={{
+                maxWidth: "100%",
+                height: "auto",
+              }}
+              // unoptimized
+            />
+          </>
+        </Link>
+      );
+    })}
+  </div>
+);
+
+export default Socials;
