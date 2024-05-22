58a129b876427cf4ad0b4aea5ae79b6a6419e220
oleksandrkever <oleksandr.kever@fourmeta.com>
2024-05-22 20:47:37 +0300

    add adaptive

--- src/widgets/FeedbackSection/ui/FeedbackCard/FeedbackCard.tsx
+++ src/widgets/FeedbackSection/ui/FeedbackCard/FeedbackCard.tsx
@@ -11,8 +11,8 @@ type Props = {
 
 const FeedbackCard: FC<Props> = ({feedback}) => {
   return (
-    <div className="flex flex-col items-center w-full aspect-[9/10] bg-white text-gray-900 px-4 py-6">
-      <div className="relative w-14 aspect-square overflow-hidden rounded-full">
+    <div className="flex flex-col items-center w-full h-full bg-white text-gray-900 px-4 py-6">
+      <div className="relative w-14 aspect-square overflow-hidden rounded-full shrink-0">
         <Image
           className="object-cover"
           alt={feedback.title || ''}
@@ -22,7 +22,7 @@ const FeedbackCard: FC<Props> = ({feedback}) => {
       </div>
       <h4 className="text-violet-400 text-sm mt-3 mb-3">{feedback.title}</h4>
       <div className="w-full border border-b-violet-400 mb-3" />
-      <p className="line-clamp-[8] px-3">{feedback.description}</p>
+      <p className="line-clamp-6 text-center px-3">{feedback.description}</p>
     </div>
   )
 };
--- src/widgets/FeedbackSection/ui/FeedbackSection.tsx
+++ src/widgets/FeedbackSection/ui/FeedbackSection.tsx
@@ -2,19 +2,34 @@
 
 import { FC, useState } from "react";
 
-import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperClass, SwiperProps, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 import { CONTENT } from "@/shared/constants";
 import { FeedbackCard } from ".";
 import Image from "next/image";
+import { SwiperOptions } from "swiper/types";
 
 type Props = {
   section: typeof CONTENT['main'][number] & {type: 'feedback'};
-}
+};
 
 const FeedbackSection: FC<Props> = ({section}) => {
   const [swiper, setSwiper] = useState<SwiperClass>();
+
+  const sliderParams: SwiperOptions = {
+    spaceBetween: 50,
+    slidesPerView: 1,
+    breakpoints: {
+      760: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      }
+    }
+  }
+
   return (
     <div className="flex flex-col items-center main-container bg-violet-400 text-white">
       <span className="font-bold">{section.header}</span>
@@ -29,12 +44,9 @@ const FeedbackSection: FC<Props> = ({section}) => {
           />
         </button>
         <Swiper
+          {...sliderParams}
           onInit={e => setSwiper(e)}
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-          className="w-full"
+          className="w-full aspect-[9/10] md:aspect-[1.8] lg:aspect-[2.8]"
         >
           {section.items.map(item => (
             <SwiperSlide key={item.src}>
