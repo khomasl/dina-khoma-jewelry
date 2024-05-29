c622c616b05f78204df6b7101bfcc7782d548210
oleksandrkever <oleksandr.kever@fourmeta.com>
2024-05-29 11:46:20 +0300

    fix

--- src/widgets/Nav/ui/Menu/Menu.tsx
+++ src/widgets/Nav/ui/Menu/Menu.tsx
@@ -19,6 +19,17 @@ const Menu = () => {
           </Link>
         );
       })}
+      {/* {Object.entries(navItems).map(([path, { name }]) => {
+        return (
+          <Link
+            key={path}
+            href={path}
+            className="text-3xl transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-2"
+          >
+            {name}
+          </Link>
+        );
+      })} */}
     </div>
   );
 };
